fed1a058eaafc605169a8221d785bd0f0804e90f
Termi1965 <62657743+Termi1965@users.noreply.github.com>
2023-08-23 23:08:25 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,65 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    while (Dealer < Count) {
+        Dealer += randint(1, 10)
+        mySprite2.sayText(convertToText(Dealer))
+        pause(200)
+        if (Dealer > 21) {
+            info.changeScoreBy(1)
+            game.gameOver(true)
+        }
+    }
+    info.changeScoreBy(-1)
+    game.gameOver(false)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Count += randint(1, 10)
+    mySprite.sayText(convertToText(Count))
+    if (Count > 21) {
+        info.changeScoreBy(-1)
+        game.gameOver(false)
+    }
+})
+let Dealer = 0
+let Count = 0
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.x = 20
+mySprite2.x = 40
+Count = 0
+Dealer = 0
